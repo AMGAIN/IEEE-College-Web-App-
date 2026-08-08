d8f7f40c2c167523d6882ab1716ec4902362c760
Amgain <surajamgain9@gmail.com>
2026-08-08 23:44:48 +0545

    event added just structure

--- ieee-nest/src/app.module.ts
+++ ieee-nest/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GalleryModule } from './gallery/gallery.module';
+import { GalleryModule } from './module/gallery/gallery.module';
+import { EventModule } from './module/event/event.module';
 
 @Module({
-  imports: [GalleryModule],
+  imports: [GalleryModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
