1a69aa2ad590409920f85482e3d29aa762ecc468
Amgain <surajamgain9@gmail.com>
2026-08-09 22:55:10 +0545

    structure for event is added

--- ieee-nest/src/app.module.ts
+++ ieee-nest/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GalleryModule } from './module/gallery/gallery.module';
 import { EventModule } from './module/event/event.module';
+import { NewsModule } from './module/news/news.module';
 
 @Module({
-  imports: [GalleryModule, EventModule],
+  imports: [GalleryModule, EventModule, NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
