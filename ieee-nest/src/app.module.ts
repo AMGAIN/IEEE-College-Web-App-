4956b44222c5fa8ab8850553e54671b49c0cca02
Amgain <surajamgain9@gmail.com>
2026-08-10 21:11:44 +0545

    team module added

--- ieee-nest/src/app.module.ts
+++ ieee-nest/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { GalleryModule } from './module/gallery/gallery.module';
 import { EventModule } from './module/event/event.module';
 import { NewsModule } from './module/news/news.module';
+import { TeamModule } from './module/team/team.module';
 
 @Module({
-  imports: [GalleryModule, EventModule, NewsModule],
+  imports: [GalleryModule, EventModule, NewsModule, TeamModule],
   controllers: [AppController],
   providers: [AppService],
 })
