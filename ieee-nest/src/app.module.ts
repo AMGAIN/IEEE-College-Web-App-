808ebe9391401b6827830d74313bef939fdc78f4
Amgain <surajamgain9@gmail.com>
2026-08-11 23:20:18 +0545

    membership's structure

--- ieee-nest/src/app.module.ts
+++ ieee-nest/src/app.module.ts
@@ -5,9 +5,10 @@ import { GalleryModule } from './module/gallery/gallery.module';
 import { EventModule } from './module/event/event.module';
 import { NewsModule } from './module/news/news.module';
 import { TeamModule } from './module/team/team.module';
+import { MembershipModule } from './module/membership/membership.module';
 
 @Module({
-  imports: [GalleryModule, EventModule, NewsModule, TeamModule],
+  imports: [GalleryModule, EventModule, NewsModule, TeamModule, MembershipModule],
   controllers: [AppController],
   providers: [AppService],
 })
